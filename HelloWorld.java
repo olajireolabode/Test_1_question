80efedefef613b6043257ada10c151f1141f83b4
olajireolabode <98364070+olajireolabode@users.noreply.github.com>
2023-01-24 10:24:04 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) {       
+        System.out.println("Hello World");    
     }
     
 }
