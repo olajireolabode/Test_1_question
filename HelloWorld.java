d6301f87b75ba3da24a3ee38a231855289fab236
abusharh <122312868+abusharh@users.noreply.github.com>
2023-01-24 10:16:28 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+       
     }
     
 }
